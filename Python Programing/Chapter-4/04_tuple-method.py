e0f979aeee2943b1e5e640656e1b9fa45c80cc83
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2025-12-11 21:40:15 +0530

    main

--- Python Programing/Chapter-4/04_tuple-method.py
+++ Python Programing/Chapter-4/04_tuple-method.py
@@ -1,4 +1,5 @@
 a = (0,4,4,5,6,7,8,False, "Rohan",1, "Shivam", 23.6, )
+b = ("Geetansh Patle", 932.21, 31, 'This is the cod e to find the error')
 print(a)
 print(type(a))
 no = a.count(4)
@@ -9,3 +10,7 @@ print(a.index(23.6))
 
 i = a.index("Rohan")
 print(i)
+
+i2 = a.list()
+print(i2)
+print(1)
