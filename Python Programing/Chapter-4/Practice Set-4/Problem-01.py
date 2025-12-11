80bd4e7cc2ec8a6236434b3eab2073634ea4466d
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2025-12-11 22:55:56 +0530

    main

---
+++ Python Programing/Chapter-4/Practice Set-4/Problem-01.py
@@ -0,0 +1,28 @@
+fruits = []
+
+
+f1 = input("Enter fruits name :"  )
+fruits.append(f1)
+
+f2 = input("Enter fruits name :"  )
+fruits.append(f2)
+
+f3 = input("Enter fruits name :"  )
+fruits.append(f3)
+
+f4 = input("Enter fruits name :"  )
+fruits.append(f4)
+
+f5 = input("Enter fruits name :"  )
+fruits.append(f5)
+ 
+f6 = input("Enter fruits name :"  )
+fruits.append(f6)
+
+f7 = input("Enter fruits name :"  )
+fruits.append(f7)
+
+f8 = input("Enter fruits name :"  )
+fruits.append(f8)
+
+print(fruits)
