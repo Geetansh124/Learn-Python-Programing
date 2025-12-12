fcd678fdaa58029a11c00d130d0dfebadfabee58
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2025-12-12 21:14:26 +0530

    main

---
+++ Python Programing/Chapter-4/Practice Set-4/Problen-02.py
@@ -0,0 +1,25 @@
+Marks = []
+
+
+f1 = input("Enter Marks here 1:"  )
+Marks.append(f1)
+
+f2 = input("Enter Marks here 2:"  )
+Marks.append(f2)
+
+f3 = input("Enter Marks here 3:"  )
+Marks.append(f3)
+
+f4 = input("Enter Marks here 4:"  )
+Marks.append(f4)
+
+f5 = input("Enter Marks here 5:"  )
+Marks.append(f5)
+ 
+f6 = input("Enter Marks here 6:"  )
+Marks.append(f6)
+
+Marks.sort()
+
+print(Marks)
+            
