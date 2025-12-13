cf4af8040b278630f1596f69b73e662b3f24e2a1
Opera Point <119775217+Operapoint56@users.noreply.github.com>
2025-12-13 19:12:40 +0530

    main

--- Python Programing/Chapter-4/Practice Set-4/Problen-02.py
+++ Python Programing/Chapter-4/Practice Set-4/Problen-02.py
@@ -1,22 +1,22 @@
 Marks = []
 
 
-f1 = input("Enter Marks here 1:"  )
+f1 = int(input("Enter Marks here 1:"  ))
 Marks.append(f1)
 
-f2 = input("Enter Marks here 2:"  )
+f2 = int(input("Enter Marks here 2:"  ))
 Marks.append(f2)
 
-f3 = input("Enter Marks here 3:"  )
+f3 = int(input("Enter Marks here 3:"  ))
 Marks.append(f3)
 
-f4 = input("Enter Marks here 4:"  )
+f4 = int(input("Enter Marks here 4:"  ))
 Marks.append(f4)
 
-f5 = input("Enter Marks here 5:"  )
+f5 = int(input("Enter Marks here 5:"  ))
 Marks.append(f5)
  
-f6 = input("Enter Marks here 6:"  )
+f6 = int(input("Enter Marks here 6:"  ))
 Marks.append(f6)
 
 Marks.sort()
